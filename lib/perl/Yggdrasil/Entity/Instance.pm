46b763433c25a69f768dba1dcf6280d0177f580d
Terje Kvernes <terje.kvernes@gmail.com>
2008-07-27 20:45:39 +0000

    Temp commit

--- lib/perl/Yggdrasil/Entity/Instance.pm
+++ lib/perl/Yggdrasil/Entity/Instance.pm
@@ -1,5 +1,7 @@
 package Yggdrasil::Entity::Instance;
 
+use base 'Yggdrasil';
+
 use strict;
 use warnings;
 
@@ -11,6 +13,32 @@ sub new {
   bless $self, $class;
 
   return $self;
+}
+
+sub define {
+    
+}
+
+sub property {
+    my $self = shift;
+    my ($key, $value) = @_;
+
+    my $storage = $self->storage();
+
+    my $entity = (split '::', ref $self)[-1];    
+    my $name = join("_", $entity, $key );
+      
+    if ($value) {
+
+	my $id = $storage->dosql_update(
+qq<INSERT INTO [name] (id, value, start) VALUES(?, ?, NOW())>, 
+ $self, [$entity->{id}, $value] );
+    
+}
+					
+	return $storage->
+    }
+    
 }
 
 1;
