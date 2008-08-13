525848048259614eb3e9f744243a357d099e7108
Terje Kvernes <terje.kvernes@gmail.com>
2008-08-13 12:04:07 +0000

    * Added a SHA1 mapper for the heck of it.

---
+++ lib/Yggdrasil/Storage/Mapper/SHA1.pm
@@ -0,0 +1,25 @@
+package Yggdrasil::Storage::Mapper::SHA1;
+
+use strict;
+use warnings;
+
+use Digest::SHA1 qw(sha1_hex);
+
+sub new {
+    my $class = shift;
+    my $self  = {};
+    
+    return bless $self, $class;
+}
+
+
+sub map :method {
+    my $self  = shift;
+    my $tomap = shift;
+    
+    my $digest = sha1_hex( $tomap );
+    $digest =~ y/0-9a-f/a-p/;
+    return $digest;
+}
+
+1;
