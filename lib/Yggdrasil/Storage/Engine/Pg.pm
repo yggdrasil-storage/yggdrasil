bd5add83c6fba5ff22640458b205d9e1c000827a
Terje Kvernes <terje.kvernes@gmail.com>
2008-08-21 23:36:46 +0000

    * Added casts to avoid having to declare functions for   UNIX_TIMESTAMP like support.

--- lib/Yggdrasil/Storage/Engine/Pg.pm
+++ lib/Yggdrasil/Storage/Engine/Pg.pm
@@ -10,29 +10,9 @@ use base 'Yggdrasil::Storage::Engine::Shared::SQL';
 use DBI;
 
 our %TYPEMAP = (
-		DATE   => 'TIMESTAMP',
+		DATE   => 'TIMESTAMP WITH TIME ZONE',
 		BINARY => 'BYTEA',
 	       );
-
-our %FUNCTIONS = (
-		  "from_unixtime:1" => "
-CREATE OR REPLACE FUNCTION from_unixtime(integer) RETURNS timestamp AS '
-SELECT
-\$1::abstime::timestamp without time zone AS result
-' LANGUAGE 'SQL';",
-		  
-		  "unix_timestamp:0"   => "
-CREATE OR REPLACE FUNCTION unix_timestamp() RETURNS integer AS '
-SELECT
-ROUND(EXTRACT( EPOCH FROM abstime(now()) ))::int4 AS result;
-' LANGUAGE 'SQL';",
-		  
-		  "unix_timestamp:1" => "		  
-CREATE OR REPLACE FUNCTION unix_timestamp(timestamp with time zone) RETURNS integer AS '
-SELECT
-ROUND(EXTRACT( EPOCH FROM ABSTIME(\$1) ))::int4 AS result;
-' LANGUAGE 'SQL';"
-);
   
 sub new {
   my $class = shift;
@@ -43,32 +23,7 @@ sub new {
   
   $self->{dbh} = DBI->connect( "DBI:Pg:database=$data{db};host=$data{host};port=$data{port}", $data{user}, $data{password}, { RaiseError => 0 } );
 
-  $self->_init();
-
   return $self;
-}
-
-sub _init {
-    my $self = shift;
-
-    my $dbh = $self->{dbh};
-    my $sth = $dbh->prepare( "select proname,pronargs from pg_proc where proname LIKE '%unix%'" );
-    confess( "no sth? " . $dbh->errstr ) unless $sth;
-    $sth->execute() || confess( "execute??" );
-
-    my $data = $sth->fetchall_arrayref();
-    my %proc_exists;
-    
-    for my $proc (@$data) {
-	my ($name, $args) = ($proc->[0], $proc->[1] || 0);
-	$proc_exists{"$name:$args"} = 1;
-    } 
-    
-    for my $proc (qw|unix_timestamp:0 unix_timestamp:1 from_unixtime:1|) {
-	unless ($proc_exists{$proc}) {
-	    $dbh->do( $FUNCTIONS{$proc} );
-	}
-    }   
 }
 
 sub _structure_exists {
@@ -111,51 +66,11 @@ sub _null_comparison_operator {
     return 'is';
 }
 
-1;
-
-__DATA__
-
-sub _get_last_id {
-    my $self = shift;
-    my $schema = shift;
-    my $table = shift;
-    
-    if ($schema =~ /insert into (\w+)\(/i) {
-        $table = $1;
-    }
-    
-    return $self->{dbh}->last_insert_id( undef, undef, lc $table, undef );
-}
-
-sub _table_filter {
-    my $self = shift;
-    my $sql  = shift;
-
-    $sql =~ s/DATETIME/TIMESTAMP/g;
-    $sql =~ s/INT NOT NULL AUTO_INCREMENT/BIGSERIAL/g;
-    $sql =~ s/(\w+)\(\d+\)/$1/g;
-    
-    return $sql;
-}
-
-# This exists to fix case issues in Postgresql, ie, the
-# 'relation "MyTable" does not exist' annoyances
-sub _update_filter {
-    my $self = shift;
-    my $sql  = shift;
-
-    $sql =~ s/^UPDATE (\w+)//;
-    my $table = $1;
-    $sql = "UPDATE " . lc $table . " $sql";
-    return $sql;
-}
-
-
 sub _time_as_epoch {
     my $self = shift;
     my $time = shift;
 
-    return "extract(epoch FROM date('$time'))";
+    return "(floor(extract(epoch FROM ($time)::timestamp with time zone)))";
 }
 
 1;
