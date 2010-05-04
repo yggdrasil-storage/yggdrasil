4307f7df9a171985b1aba4ac0612893fe34fb66b
Terje Kvernes <terje.kvernes@gmail.com>
2010-05-04 23:45:35 +0000

    * Complete interface required to display a property.

--- lib/Yggdrasil/Remote/Property.pm
+++ lib/Yggdrasil/Remote/Property.pm
@@ -36,4 +36,24 @@ sub entity {
     return bless $dataref, 'Yggdrasil::Remote::Entity';
 }
 
+sub name {
+    my $self = shift;
+    return $self->{name};
+}
+
+sub null {
+    my $self = shift;
+    return $self->_get_meta( 'null', @_ );
+}
+
+sub type {
+    my ($self, $property) = (shift, shift);
+    return $self->_get_meta( 'type', @_ );
+}
+
+sub _get_meta {
+    my ($self, $type) = (shift, shift);
+    return $self->storage()->{protocol}->get_property_meta( $self->{entity}, $self->{name}, $type );
+}
+
 1;
