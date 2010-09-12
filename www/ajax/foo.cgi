36dcaefa288bc6323405b3aee05dd544bf1a48c1
Terje Kvernes <terje.kvernes@gmail.com>
2010-09-12 21:17:31 +0000

    * Initial AJAX interface.
    * Mousing over the Yggdrasil link will open a list of Entities.
    Clicking on an entity will expand it to show instances.
    Double-clicking will open the entity itself.  Title links inform
    the user about the UI.
    
    * Overflow isn't handled yet.

--- www/ajax/foo.cgi
+++ www/ajax/foo.cgi
@@ -7,33 +7,117 @@ use FindBin qw($Bin);
 use lib qq($Bin/../../lib);
 
 use Yggdrasil;
+use Yggdrasil::Common::Config;
 use CGI::Pretty qw/-debug/;
 
 use JSON;
 
-my $json = JSON->new( pretty => 1 );
+my $json = JSON->new();
 my $cgi = CGI::Pretty->new();
 
 print "Content-Type: application/json\n\n";
 
+my $c = Yggdrasil::Common::Config->new();
 my $y = Yggdrasil->new();
-$y->connect( user     => "yggdrasil", 
-	     password => "beY6KAAVNbhPa6SP",
 
-	     host   => "db.math.uio.no",
-	     db     => "yggdrasil",
-	     engine => "mysql",
-    );
+my $config = $c->get( 'web' );
+die "Unable to load the web configuration" unless $config;
+
+my $yhost  = $config->get( 'enginehost' );
+
+$y->connect( user     => $config->get( 'engineuser' ),
+	     password => $config->get( 'enginepassword' ),
+
+	     host   => $yhost,
+	     db     => $config->get( 'enginedb' ),
+	     engine => $config->get( 'enginetype' ),
+	   );
 
 my $sess = $cgi->cookie( "sessionID" );
 my $u = $y->login( user => undef, password => undef, session => $sess );
 unless( $u ) {
-    my $error = { error => "I no like your session" };
-    print $json->objToJson($error);
+    &error( 'I no like your session' );
     exit;
 
 }
 
-my $result = { happy => "happy, happy",
-	       joy   => "joy, joy" };
-print $json->objToJson($result);
+my $mode  = $cgi->param( 'mode' ) || '';
+my $start = $cgi->param( 'start' ) || '';
+my $stop  = $cgi->param( 'stop' ) || '';
+
+my $entity   = $cgi->param( 'entity' ) || '';
+my $instance = $cgi->param( 'instance' ) || '';
+my $property = $cgi->param( 'property' ) || '';
+
+if ($mode eq 'entities') {
+    exec_entities();
+} elsif ($mode eq 'relations') {
+    exec_relations( $cgi->param( 'entity' ));
+} elsif ($entity) {
+    exec_entity( $entity );
+} elsif ($instance) {
+    exec_instance( $entity, $instance );
+} elsif ($property) {
+    exec_property( $entity, $property );
+} else {
+    error( 'Unknown query' );
+}
+
+
+
+sub exec_entities {
+    my @data = map { nameify( $_ ) } $y->entities();
+    print $json->encode( \@data );
+}
+  
+sub exec_relations {
+    my $entity = shift;
+
+    if ($entity) {
+	&error( 'Getting relations for a specific entity is not supported' ); 
+    } else {
+	my @data = map { nameify( $_ ) } $y->relations();
+	print $json->encode( \@data );	
+    }
+}
+  
+sub exec_entity {
+    my $entity = shift;
+    
+    my $eobj = $y->get_entity( $entity );
+    
+    unless ($eobj) {
+	&error( $y->get_status()->message() );
+	return;
+    }
+
+    my @i = map { nameify( $_ ) } $eobj->instances();
+    my @p = map { nameify( $_ ) } $eobj->properties();
+    
+    print $json->encode( [ \@i, \@p ] );
+}
+
+sub exec_instance {
+    &error( 'Not implemented' );
+}
+  
+sub exec_property {
+    &error( 'Not implemented' );
+}
+  
+sub error {
+    my $string = shift;
+    print $json->encode( { error => $string } );
+}
+
+
+sub nameify {
+    my $obj = shift;
+    
+    if ($obj->stop()) {
+        return $obj->id() . ':' . $obj->start() . '-' . $obj->stop();
+    } else {
+        return $obj->id();
+    }
+}
+
--- www/yggdrasil.js
+++ www/yggdrasil.js
@@ -9,11 +9,145 @@ function flipper(one, two) {
     $(id_two).css( "display", two == '' || two == 'block' ? 'none' : 'block' );
 }
 
+function showStructure() {
+    var entities;
+    var query = 'entities';
+    var data;
 
-function doSecret() {
-    var cb = function(data) {
-	$("#secret").html( data["happy"] + ". " + data["joy"] );
+
+    if ( $('ul.entitylist').length > 0 ) {
+	// We havea previous entry in the code, remove it and start
+	// over.  Timer?
+	$('#entitydetails').remove();
+	$('ul.entitylist').remove();
     }
 
-    $.post( "ajax/foo.cgi", {}, cb, "json" );
+    $("a#structure").attr ('href', '#');
+
+    if ( ! $('#entities').length ) {
+	$('#container').append('<div id="entities" title="Click to close navigator">' +
+			       '<img src="images/close.png" title="Close" id="closer" heigh="10" width="13" /><h3>Entities</h3></div>'); 
+    }
+
+    $('#entities').show( 100 );
+
+    var cb = function(data) {
+	process( query, data );	
+    }
+
+    var hideme = function() {
+	$('#entitydetails').remove();
+	$('#entities').hide( 300 );
+    }
+
+    $.post( "ajax/foo.cgi", { mode: query }, cb, "json" );
+
+    $('#entities h3').click( hideme );
+    $('#closer').click( hideme );
+    // $('#entities h3').mouseover( showclose );
+    // $('#entities h3').mouseout( shownormal );
+
+}
+
+function showclose() {  
+    $('#entities h3').html( 'Close' );
+}
+
+function shownormal() {  
+    $('#entities h3').html( 'Entities' );
+}
+
+function process( q, d ) {
+    var query = q;
+    var input = d;
+
+    if (query == 'entities') {
+	var list = document.createElement( 'ul' );
+	list.className = 'entitylist';
+	for (var i=0; i<input.length; i++) {
+	    var newPara = document.createElement( 'li' );
+	    var newParaText = document.createTextNode( input[i] );
+	    newPara.setAttribute( 'title', "Click to expand, double click to display" );
+	    newPara.appendChild( newParaText );
+	    list.appendChild( newPara );
+	}
+	$('#entities').append( list );
+    }
+    $('#entities ul li').click( doentity );
+    $('#entities ul li').dblclick( gotoEntity );
+}
+
+function gotoEntity() {
+    var entity = this.innerHTML;
+    
+    window.location = '?entity=' + entity;
+}
+
+function doentity() {
+    var entity = this.innerHTML;
+
+    var entitycb = function(data) {
+	processEntity( entity, data );	
+    }
+
+    $.post( "ajax/foo.cgi", { entity: entity }, entitycb, "json" );
+}
+
+function processEntity( e, data ) {
+    var input = data;
+    
+    var instances  = data[0];
+    var properties = data[1];
+
+    var ewidth = $('#entities').width();
+
+    $('#entitydetails').remove();
+    $('#container').append('<div id="entitydetails"></div>' );
+
+    $('#entitydetails').css( 'position', 'absolute' );
+    $('#entitydetails').css( 'top', '50px' );
+    $('#entitydetails').css( 'left', ewidth + 50 );
+
+    $('#entitydetails').show( 100 );
+
+    if (! data[0].length && ! data[1].length ) {
+	$('#entitydetails').append( '(empty)' );
+	return;
+    }
+
+    //    for (var type=0; type<input.length; type++) {
+    for (var type=0; type<1; type++) {
+ 	if (data[type].length) {
+	    var list = document.createElement( 'ul' );
+ 	    if (type == 0) {
+ 		list.className = 'instancelist';
+		$('#entitydetails').append('<h4>Instances</h4>' );
+ 	    } else {
+ 		list.className = 'propertylist';
+		$('#entitydetails').append('<h4>Properties</h4>' );
+ 	    }	    
+	    
+ 	    for (var i=0; i<data[type].length; i++) {
+ 		var newPara = document.createElement( 'li' );
+ 		var newParaText = document.createTextNode( data[type][i] );
+		newPara.setAttribute( 'title', "Click to display" );	    
+ 		newPara.appendChild( newParaText );
+ 		list.appendChild( newPara );
+ 	    }
+	    $('#entitydetails').append( list );
+ 	}
+     }
+
+    $('#entitydetails ul li').click( function() { doInstanceOrProperty( e, this ) } );
+}
+
+function doInstanceOrProperty( e, li ) {
+    var entity = e;
+    var label  = li.innerHTML;
+
+    window.location = '?entity=' + entity + ';instance=' + label;
+
+
+    // window.location
+    
 }
